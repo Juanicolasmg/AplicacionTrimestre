5840b8046e185d73926e044c2118e91828abed9e
Juanicolasmg <juanicolasmorenog@gmail.com>
2021-08-10 12:45:34 -0500

    Creando CRUD Tabla Roles

---
+++ AplicaciónTrimestre/Controllers/RolesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AplicaciónTrimestre.Models;
+
+namespace AplicaciónTrimestre.Controllers
+{
+    public class RolesController : Controller
+    {
+        // GET: Roles
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.roles.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(roles roles)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.roles.Add(roles);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                var findRoles = db.roles.Find(id);
+                return View(findRoles);
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var findRoles = db.roles.Find(id);
+                    db.roles.Remove(findRoles);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    roles findRoles = db.roles.Where(a => a.id == id).FirstOrDefault();
+                    return View(findRoles);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(roles editRoles)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    roles user = db.roles.Find(editRoles.id);
+
+                    user.descripcion = editRoles.descripcion;
+                    
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+    }
+}
