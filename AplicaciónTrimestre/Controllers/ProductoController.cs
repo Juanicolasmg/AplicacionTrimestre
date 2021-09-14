7cc4cec9fd0bf562d6d125ad15ecd69d14a0b2e1
Juanicolasmg <juanicolasmorenog@gmail.com>
2021-09-13 21:42:43 -0500

    Creando Vista Plantilla, Creando Modelo Custom Reporte (Producto-Proveedor)  y utilizandolo

--- AplicaciónTrimestre/Controllers/ProductoController.cs
+++ AplicaciónTrimestre/Controllers/ProductoController.cs
@@ -134,5 +134,29 @@ namespace AplicaciónTrimestre.Controllers
                 return RedirectToAction("Index");
             }
         }
+
+        public ActionResult Reporte()
+        {
+            try
+            {
+                var db = new inventario2021Entities();
+                var query = from tabProveedor in db.proveedor
+                            join tabProducto in db.producto on tabProveedor.id equals tabProducto.id_proveedor
+                            select new Reporte
+                            {
+                                nombreProveedor = tabProveedor.nombre,
+                                telefonoProveedor = tabProveedor.telefono,
+                                direccionProveedor = tabProveedor.direccion,
+                                nombreProducto = tabProducto.nombre,
+                                precioProducto = tabProducto.percio_unitario
+                            };
+                return View(query);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
     }
 }
